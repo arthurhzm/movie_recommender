8d44eaa7d5e9ab9cfc232b8990f17a65bd7f44d5
Arthur Marena <arthurzhm2@gmail.com>
2025-04-10 17:39:38 -0300

    FEAT: Drawer Component

---
+++ movie_recommender/lib/components/drawer_component.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+class DrawerComponent extends StatelessWidget {
+  const DrawerComponent({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: ListView(
+        padding: EdgeInsets.zero,
+        children: [
+          const DrawerHeader(
+            decoration: BoxDecoration(color: Colors.blue),
+            child: Text(
+              'Menu',
+              style: TextStyle(color: Colors.white, fontSize: 24),
+            ),
+          ),
+          ListTile(
+            leading: const Icon(Icons.home),
+            title: const Text('Início'),
+            onTap: () {
+              Navigator.pushNamed(context, '/home');
+            },
+          ),
+          ListTile(
+            leading: const Icon(Icons.logout),
+            title: const Text('Sair'),
+            onTap: () {
+              Navigator.pop(context);
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}

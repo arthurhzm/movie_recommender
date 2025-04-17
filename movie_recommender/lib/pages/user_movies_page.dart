6c350b9ad0b5260c4ae8d333105b107b8ccfb716
Arthur Marena <arthurzhm2@gmail.com>
2025-04-17 18:04:36 -0300

    FEAT: Add UserMoviesPage with initial structure and placeholder

---
+++ movie_recommender/lib/pages/user_movies_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class UserMoviesPage extends StatefulWidget {
+  const UserMoviesPage({super.key});
+
+  @override
+  State<UserMoviesPage> createState() => _UserMoviesPageState();
+}
+
+class _UserMoviesPageState extends State<UserMoviesPage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}

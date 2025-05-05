ff77a71c20fad1df7800ab38f1674aa586b33b75
Guilherme Bisaio <guilhermebisaio.curric@gmail.com>
2025-05-05 17:58:37 -0300

    FEAT: Adicionado texto de boas-vindas ao login

--- movie_recommender/lib/pages/login_page.dart
+++ movie_recommender/lib/pages/login_page.dart
@@ -33,11 +33,28 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Login Page')),
+      appBar: AppBar(title: const Text('CineMatch')),
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: Column(
           children: [
+            Icon(Icons.movie_filter, size: 100),
+            RichText(
+              textAlign: TextAlign.center,
+              text: TextSpan(
+                style: Theme.of(context).textTheme.headlineMedium,
+                children: <TextSpan>[
+                  TextSpan(
+                    text: "Bem-vindo ao CineMatch!\n",
+                    style: TextStyle(fontWeight: FontWeight.bold, fontSize: 40),
+                  ),
+                  TextSpan(
+                    text: "Faça seu login para começar sua busca pelo filme perfeito para você",
+                    style: TextStyle(fontSize: 18),
+                  ),
+                ],
+              ),
+            ),
             TextField(
               controller: _emailController,
               decoration: const InputDecoration(labelText: 'Email'),

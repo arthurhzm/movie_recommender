ae3f57a6342f2567225fba0782c5714cc477c871
Arthur Marena <arthurzhm2@gmail.com>
2025-05-16 17:16:28 -0300

    FEAT: integrate GeminiProvider for personalized movie recommendations on HomePage

--- movie_recommender/lib/pages/home_page.dart
+++ movie_recommender/lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:movie_recommender/components/drawer_component.dart';
+import 'package:movie_recommender/providers/gemini_provider.dart';
 import 'package:movie_recommender/services/user_service.dart';
 import 'package:movie_recommender/components/standard_button.dart';
 import 'package:movie_recommender/components/standard_appbar.dart';
@@ -19,12 +20,15 @@ class _HomePageState extends State<HomePage> {
   final userId = FirebaseAuth.instance.currentUser?.uid;
   final user = FirebaseAuth.instance.currentUser;
   final UserService _userService = UserService();
+  final GeminiProvider _geminiProvider = GeminiProvider();
   late Future<Map<String, dynamic>> userPreferences;
+  late Future<List<Map<String, dynamic>>> movies;
 
   @override
   void initState() {
     super.initState();
     userPreferences = _userService.getUserPreferences();
+    movies = _geminiProvider.getMoviesRecommendations(10);
   }
 
   @override
@@ -44,6 +48,7 @@ class _HomePageState extends State<HomePage> {
             icon: const Icon(Icons.logout),
             onPressed: () async {
               await FirebaseAuth.instance.signOut();
+              if (!mounted) return;
               Navigator.pushReplacementNamed(context, '/');
             },
           ),
@@ -62,95 +67,213 @@ class _HomePageState extends State<HomePage> {
             });
             return const Center(child: Text('Redirecting...'));
           } else {
-            return Center(
-              child: Column(
-                children: [
-                  const SizedBox(height: 20,),
-                  Text(
-                    "${horarioAtual()} ${user?.displayName ?? ""}, vamos escolher um filme para assistir?",
-                  ),
-                  const SizedBox(height: 15),
-                  Text("Filmes com base em seus gostos"),
-                  SizedBox(
-                    height: 180,
-                    child: ScrollConfiguration(
-                      behavior: ScrollConfiguration.of(context).copyWith(
-                        scrollbars: true,
-                        dragDevices: {
-                          PointerDeviceKind.touch,
-                          PointerDeviceKind.mouse,
-                          PointerDeviceKind.trackpad,
-                        },
-                      ),
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 10,
-                        physics: const BouncingScrollPhysics(),
-                        padding: EdgeInsets.symmetric(horizontal: 16),
-                        itemBuilder: (context, index) {
-                          return Container(
-                            width: 120,
-                            margin: EdgeInsets.only(right: 10),
-                            decoration: BoxDecoration(
-                              color: Colors.grey[300],
-                              borderRadius: BorderRadius.circular(4),
+            return FutureBuilder(
+              future: movies,
+              builder: (context, snapshot) {
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (snapshot.hasError) {
+                  return Center(child: Text('Error: ${snapshot.error}'));
+                } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+                  WidgetsBinding.instance.addPostFrameCallback((_) {
+                    Navigator.pushNamed(context, '/preferences/add');
+                  });
+                  return const Center(child: Text('Redirecting...'));
+                } else {
+                  return Center(
+                    child: Column(
+                      children: [
+                        const SizedBox(height: 20),
+                        Text(
+                          "${horarioAtual()} ${user?.displayName ?? ""}, vamos escolher um filme para assistir?",
+                        ),
+                        const SizedBox(height: 15),
+                        Text("Filmes com base em seus gostos"),
+                        SizedBox(
+                          height: 180,
+                          child: ScrollConfiguration(
+                            behavior: ScrollConfiguration.of(context).copyWith(
+                              scrollbars: true,
+                              dragDevices: {
+                                PointerDeviceKind.touch,
+                                PointerDeviceKind.mouse,
+                                PointerDeviceKind.trackpad,
+                              },
                             ),
-                            child: Center(
-                              child: Text(
-                                "Item ${index + 1}",
-                                style: TextStyle(color: Colors.black),
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 10,
+                              physics: const BouncingScrollPhysics(),
+                              padding: EdgeInsets.symmetric(horizontal: 16),
+                              itemBuilder: (context, index) {
+                                final movie = snapshot.data![index];
+                                return GestureDetector(
+                                  onTap: () {
+                                    showDialog(
+                                      context: context,
+                                      builder:
+                                          (ctx) => AlertDialog(
+                                            title: Text(movie['title'] ?? ''),
+                                            content: SingleChildScrollView(
+                                              child: Column(
+                                                mainAxisSize: MainAxisSize.min,
+                                                crossAxisAlignment:
+                                                    CrossAxisAlignment.start,
+                                                children: [
+                                                  if (movie['poster_url'] !=
+                                                      null)
+                                                    Center(
+                                                      child: Image.network(
+                                                        movie['poster_url'],
+                                                        height: 200,
+                                                        fit: BoxFit.cover,
+                                                      ),
+                                                    ),
+                                                  const SizedBox(height: 12),
+                                                  Text(
+                                                    "${movie['year']} • ${movie['genres']?.join(', ') ?? ''}",
+                                                    style: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                  const SizedBox(height: 12),
+                                                  const Text(
+                                                    'Sinopse',
+                                                    style: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                  const SizedBox(height: 4),
+                                                  Text(
+                                                    movie['overview'] ??
+                                                        'Sinopse não disponível.',
+                                                  ),
+                                                  const SizedBox(height: 12),
+                                                  const Text(
+                                                    'Por que recomendamos:',
+                                                    style: TextStyle(
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                  const SizedBox(height: 4),
+                                                  Text(
+                                                    movie['why_recommend'] ??
+                                                        '',
+                                                  ),
+                                                  const SizedBox(height: 8),
+                                                  Text(
+                                                    'Disponível em: ${movie['streaming_services']?.join(', ') ?? 'Nenhum serviço de streaming encontrado'}',
+                                                    style: TextStyle(
+                                                      color: Colors.blue,
+                                                      fontWeight:
+                                                          FontWeight.bold,
+                                                    ),
+                                                  ),
+                                                ],
+                                              ),
+                                            ),
+                                            actions: [
+                                              TextButton(
+                                                onPressed:
+                                                    () =>
+                                                        Navigator.of(ctx).pop(),
+                                                child: const Text('Fechar'),
+                                              ),
+                                            ],
+                                          ),
+                                    );
+                                  },
+                                  child: Container(
+                                    width: 120,
+                                    margin: EdgeInsets.only(right: 12),
+                                    decoration: BoxDecoration(
+                                      color: Colors.grey[200],
+                                      borderRadius: BorderRadius.circular(8),
+                                      boxShadow: [
+                                        BoxShadow(
+                                          color: Colors.black12,
+                                          blurRadius: 4,
+                                          offset: Offset(0, 2),
+                                        ),
+                                      ],
+                                    ),
+                                    child: Column(
+                                      crossAxisAlignment:
+                                          CrossAxisAlignment.start,
+                                      children: [
+                                        ClipRRect(
+                                          borderRadius: BorderRadius.vertical(
+                                            top: Radius.circular(8),
+                                          ),
+                                          child: Image.network(
+                                            movie['poster_url'],
+                                            height: 180,
+                                            width: 120,
+                                            fit: BoxFit.cover,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        Text("Filmes festivos para você"),
+                        SizedBox(
+                          height: 180,
+                          child: ScrollConfiguration(
+                            behavior: ScrollConfiguration.of(context).copyWith(
+                              scrollbars: true,
+                              dragDevices: {
+                                PointerDeviceKind.touch,
+                                PointerDeviceKind.mouse,
+                                PointerDeviceKind.trackpad,
+                              },
+                            ),
+                            child: ListView.builder(
+                              scrollDirection: Axis.horizontal,
+                              itemCount: 10,
+                              physics: const BouncingScrollPhysics(),
+                              padding: EdgeInsets.symmetric(horizontal: 16),
+                              itemBuilder: (context, index) {
+                                return Container(
+                                  width: 120,
+                                  margin: EdgeInsets.only(right: 10),
+                                  decoration: BoxDecoration(
+                                    color: Colors.grey[300],
+                                    borderRadius: BorderRadius.circular(4),
+                                  ),
+                                  child: Center(
+                                    child: Text(
+                                      "Item ${index + 1}",
+                                      style: TextStyle(color: Colors.black),
+                                    ),
+                                  ),
+                                );
+                              },
+                            ),
+                          ),
+                        ),
+                        const SizedBox(height: 15),
+                        StandardButton(
+                          onPressed:
+                              () => Navigator.pushNamed(
+                                context,
+                                '/recommendations',
                               ),
-                            ),
-                          );
-                        },
-                      ),
+                          child: const Text('Me recomende filmes'),
+                        ),
+                      ],
                     ),
-                  ),
-                  const SizedBox(height: 15),
-                  Text("Filmes festivos para você"),
-                  SizedBox(
-                    height: 180,
-                    child: ScrollConfiguration(
-                      behavior: ScrollConfiguration.of(context).copyWith(
-                        scrollbars: true,
-                        dragDevices: {
-                          PointerDeviceKind.touch,
-                          PointerDeviceKind.mouse,
-                          PointerDeviceKind.trackpad,
-                        },
-                      ),
-                      child: ListView.builder(
-                        scrollDirection: Axis.horizontal,
-                        itemCount: 10,
-                        physics: const BouncingScrollPhysics(),
-                        padding: EdgeInsets.symmetric(horizontal: 16),
-                        itemBuilder: (context, index) {
-                          return Container(
-                            width: 120,
-                            margin: EdgeInsets.only(right: 10),
-                            decoration: BoxDecoration(
-                              color: Colors.grey[300],
-                              borderRadius: BorderRadius.circular(4),
-                            ),
-                            child: Center(
-                              child: Text(
-                                "Item ${index + 1}",
-                                style: TextStyle(color: Colors.black),
-                              ),
-                            ),
-                          );
-                        },
-                      ),
-                    ),
-                  ),
-                  const SizedBox(height: 15),
-                  StandardButton(
-                    onPressed:
-                        () => Navigator.pushNamed(context, '/recommendations'),
-                    child: const Text('Me recomende filmes'),
-                  ),
-                ],
-              ),
+                  );
+                }
+              },
             );
           }
         },

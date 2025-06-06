4d7f11d40ac774f8111c7d4436b66cb8b07ecc9b
Arthur Marena <arthurzhm2@gmail.com>
2025-06-06 16:18:53 -0300

    FEAT: atualizando as coisas para rodar no celular kkkk

--- movie_recommender/android/app/build.gradle.kts
+++ movie_recommender/android/app/build.gradle.kts
@@ -11,7 +11,7 @@ plugins {
 android {
     namespace = "com.example.movie_recommender"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = flutter.ndkVersion
+    
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
@@ -27,7 +27,7 @@ android {
         applicationId = "com.example.movie_recommender"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
